f31ffc4e23eda12a064f01afae83da526511bf90
Gordon Brooks <brooksg@ohio.edu>
2023-09-18 11:22:09 -0500

    Add MIT license

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -8,3 +8,22 @@ lib_summary <- function() {
   # last line is returned from function
   pkg_df
 }
+
+#run in console
+# check()
+# run check frequently
+# must fix errors
+# should fix warnings if submit CRAN must
+# try to eliminate notes if submit CRAN must explain
+# new package undergoes manual inspection
+# or if notes
+
+#run in console
+# use_mit_license()
+# we are still creator and copyrighter but they have license to use
+# unless explicitly given world to use it, they cannot
+# so muct have license for them to use it
+# anyone author in description gets copyright
+# license.md has actual MIT license in it
+# CRAN requires LICENSE only
+# .md gets added to .Rbuildignore
