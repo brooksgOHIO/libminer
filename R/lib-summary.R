14f893877b1eb23d950c3a713df6fefe0a863c03
Gordon Brooks <brooksg@ohio.edu>
2023-09-18 14:59:27 -0500

    add tests and sizes

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -3,19 +3,37 @@
 #'Provides a brief summary of the package libraries on
 #'your machine.
 #'
+#'@param sizes logical for sizes
+#'   Default  `FALSE`
 #' @return A `data.frame` containing the count of packages in each
-#'   of the user's libraries.
+#'   of the user's libraries. If `sizes = TRUE` a column of sizes will be added
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+#' lib_summary(sizes = TRUE)
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {  # check arguments before run anything
+    stop("sizes must be logical (TRUE/FALSE)")
+  } #fail early
   pkgs <- utils::installed.packages()
   # one column is libPath
   # summarize library paths
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  #like (sizes == TRUE) or isTRUE(sizes) but since we did check above not critical
   names(pkg_df) <- c("Library", "n_packages")
+  if (isTRUE(sizes)) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1) #specifies just one number for each result
+    )
+  }
+  # NA == TRUE is NA
   # last line is returned from function
   pkg_df
 }
+
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -8,5 +8,15 @@ test_that("lib_summary returns expected results", {
 })
 
 test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
-  })
+# expect_error(lib_summary("foo"), "sizes must be logical (TRUE/FALSE)", fixed=TRUE)
+  expect_error(lib_summary("foo"), "sizes must be logical")
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_s3_class(res, "data.frame") #like class(res)
+  expect_equal(ncol(res), 3)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size")) #expected but just be sure
+  expect_type(res$Library, "character") #also checks dependencies
+  expect_type(res$lib_size, "double")
+})
