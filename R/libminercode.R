e900b80e51fb428d9c6a94eabb7110848b94e33f
Gordon Brooks <brooksg@ohio.edu>
2023-09-18 10:14:32 -0500

    Initial commit

---
+++ R/libminercode.R
@@ -0,0 +1,27 @@
+# to open new project in Rstudio before
+# ran this
+## create_package("~/Desktop/libminer")
+
+# use git to distribute
+# indispensable
+# if give to other people need git
+
+# if not already done
+## use_git_config(user.name = "Gordon Brooks", user.email = "brooksg@ohio.edu")
+
+library(devtools)
+git_vaccinate()
+git_sitrep()
+
+# ── Git global (user)
+# • Name: 'Gordon Brooks'
+# • Email: 'brooksg@ohio.edu'
+# • Global (user-level) gitignore file: <unset>
+#  • Vaccinated: TRUE
+# ℹ See `?git_vaccinate` to learn more
+# ℹ Defaulting to 'https' Git protocol
+# • Default Git protocol: 'https'
+# • Default initial branch name: 'master'
+# https is good for communication
+
+use_git()
